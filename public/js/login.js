5d4f889a96223372ccec274d5d64f11b953f9ade
rensnz <renan.goncavelves@sptech.school>
2023-11-12 11:29:17 -0300

    creating first validations for login

---
+++ public/js/login.js
@@ -0,0 +1,12 @@
+function realizarLogin(){
+    var email = input_email.value
+    var senha = input_password.value 
+
+    if(email == `renan@gmail.com` && senha == `123`){
+        window.location.href = "home.html"
+    }else if(email = "" || senha == ""){
+        alert("E-mail ou senha não preenchidos corretamente")
+    }else{
+        alert("E-mail ou senha incorretos")
+    }
+}
