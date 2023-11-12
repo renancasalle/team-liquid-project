75ec396059a2bb25a811a6d3774770e6c64749c0
rensnz <renan.goncavelves@sptech.school>
2023-11-12 18:31:16 -0300

    adding a quiz questions part

---
+++ public/js/questions.js
@@ -0,0 +1,47 @@
+const questions = [
+    {
+    question: "Em que ano foi criada a Team Liquid?",
+    answer: [
+        {text: "A) 2013", correct: false},
+        {text: "B) 2001", correct: false},
+        {text: "C) 1999", correct: false},
+        {text: "D) 2000", correct: true}
+    ]
+}, 
+{
+    question: "Quem é o atual IGL (In-game Leader) do time de Rainbow Six?",
+    answer: [
+        {text: "A) Nesk", correct: false},
+        {text: "B) Paluh", correct: false},
+        {text: "C) Lagonis", correct: true},
+        {text: "D) Volpz", correct: false},
+    ]
+}, 
+{
+    question: "Existem 2 Alienware Training Facility no mundo, onde eles estão localizados?",
+    answer: [
+        {text: "A) Utrech e São Paulo", correct: true},
+        {text: "B) Utrech e Los Angeles", correct: false},
+        {text: "C) Pequim e Camberra", correct: false},
+        {text: "D) Xique-Xique e Tokyo", correct: false},
+    ]
+},
+{
+    question: "Por que o logo da Team Liquid é um cavalo?",
+    answer: [
+        {text: "A) Por conta de uma música do Nirvana", correct: false},
+        {text: "B) Por conta da pintura Avalanche", correct: true},
+        {text: "C) Por conta de um sonho", correct: false},
+        {text: "D) Já veio assim", correct: false},
+    ]
+},
+{ 
+    question: "Quem é o fundador da Team Liquid?",
+    answer:[ 
+    {text: "A) Rafael 'Razah' Ribeiro", correct: false},
+    {text: "A) Victor 'Nazgul' Goossens", correct: true},
+    {text: "A) Jonathan 'EliGE' Jablonowski", correct: false},
+    {text: "A) Finn 'Karrigan' Andersen", correct: true},
+    ]
+}
+]
