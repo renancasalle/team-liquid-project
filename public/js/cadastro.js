a25e4b1b2636b3ac0122e192a180783fa1e80796
rensnz <renan.goncavelves@sptech.school>
2023-11-12 12:47:36 -0300

    adding first validations to registration

---
+++ public/js/cadastro.js
@@ -0,0 +1,18 @@
+function realizarCadastro(){
+    var re = /\S+@\S+\.\S+/;
+    var email = input_email.value
+    var password = input_password.value
+    var confirmPassword = input_confirmPassword.value
+    var emailTest = re.test(email)
+
+    if(!emailTest){
+        alert("Insira um E-mail válido.")
+    }else if(password == "" || confirmPassword == ""){
+        alert("É necessário preencher todos os campos")
+    }else if(password != confirmPassword){
+        alert("As senhas não coincidem.")
+    }else{
+        alert("Cadastro realizado com sucesso!")
+        window.location.href = "login.html"
+    }
+}
