5448b55d5d5b768f5e1e8833304d88e7cb593198
rensnz <renan.goncalves@sptech.school>
2023-11-23 11:05:01 -0300

    adding database script

---
+++ project/database/teamliquid-script.sql
@@ -0,0 +1,21 @@
+create database TeamLiquid;
+use TeamLiquid;
+
+create table usuario(
+idUsuario int primary key auto_increment,
+nick varchar(45),
+email varchar(45),
+senha varchar(45));
+
+create table pontuacao(
+idPontuacao int primary key auto_increment,
+pontos int,
+fkUsuario int,
+foreign key (fkUsuario) references usuario(idUsuario));
+
+create table feedback(
+idFeedback int primary key auto_increment,
+feedback varchar(200),
+fkUsuario int,
+foreign key (fkUsuario) references usuario(idUsuario));
+
