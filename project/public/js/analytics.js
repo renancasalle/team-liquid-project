134be7f7949797dca518c3a796642cd3a9bebbfe
rensnz <renan.goncavelves@sptech.school>
2023-11-26 19:46:04 -0300

    implementing graphs and data in kpi's

--- project/public/js/analytics.js
+++ project/public/js/analytics.js
@@ -14,50 +14,50 @@ function selectStar(element) {
 
     avaliable.innerHTML = ""
 
-    if (star = ""){
-        alert("É necessário avaliar o quiz para")
-    }else if(star == 1){
+    
+    if(star == 1){
         avaliable.innerHTML = `
-        <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-        <img src="assents/svg/13595-removebg-preview.svg" />
-        <img src="assents/svg/13595-removebg-preview.svg" />
-        <img src="assents/svg/13595-removebg-preview.svg" />
-        <img src="assents/svg/13595-removebg-preview.svg" />
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_174126319-removebg-preview.svg" />
+        <img src="assents/svg/imagem_2023-11-26_174126319-removebg-preview.svg" />
+        <img src="assents/svg/imagem_2023-11-26_174126319-removebg-preview.svg" />
+        <img src="assents/svg/imagem_2023-11-26_174126319-removebg-preview.svg" />
         `
     
+        
     }else if(star == 2){
         avaliable.innerHTML = `
-        <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-        <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-        <img src="assents/svg/13595-removebg-preview.svg" />
-        <img src="assents/svg/13595-removebg-preview.svg" />
-        <img src="assents/svg/13595-removebg-preview.svg" />
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_174126319-removebg-preview.svg" />
+        <img src="assents/svg/imagem_2023-11-26_174126319-removebg-preview.svg" />
+        <img src="assents/svg/imagem_2023-11-26_174126319-removebg-preview.svg" />
         `
     }else if(star == 3){
         avaliable.innerHTML = `
-        <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-        <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-        <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-        <img src="assents/svg/13595-removebg-preview.svg" />
-        <img src="assents/svg/13595-removebg-preview.svg" />
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_174126319-removebg-preview.svg" />
+        <img src="assents/svg/imagem_2023-11-26_174126319-removebg-preview.svg" />
         `
     }else if(star == 4){
         avaliable.innerHTML = `
-        <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-        <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-        <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-        <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
         <img src="assents/svg/13595-removebg-preview.svg"
         `
-    //}else{
-    //    avaliable.innerHTML = `
-    //    <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-    //    <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-    //    <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-    //    <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-    //    <img src="assents/svg/imagem_2023-11-25_231041864-removebg-preview.svg">
-    //    `
-    //}
+    }else{
+        avaliable.innerHTML = `
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        <img src="assents/svg/imagem_2023-11-26_173813836-removebg-preview.svg">
+        `
+    }
 }
-}
+
 window.addEventListener("load", openModal);
